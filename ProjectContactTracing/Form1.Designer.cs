4917583453a17e1948adf3e24d63e924a46d985b
allijasuba <allijasuba@gmail.com>
2021-06-10 00:50:13 +0800

    added text boxes

--- ProjectContactTracing/Form1.Designer.cs
+++ ProjectContactTracing/Form1.Designer.cs
@@ -36,6 +36,13 @@ namespace ProjectContactTracing
             this.lblSex = new System.Windows.Forms.Label();
             this.lblContactNumber = new System.Windows.Forms.Label();
             this.lblAddress = new System.Windows.Forms.Label();
+            this.txtbxFirstName = new System.Windows.Forms.TextBox();
+            this.txtbxMiddleInitial = new System.Windows.Forms.TextBox();
+            this.txtbxLastName = new System.Windows.Forms.TextBox();
+            this.txtbxAge = new System.Windows.Forms.TextBox();
+            this.txtbxSex = new System.Windows.Forms.TextBox();
+            this.txtbxContactNumber = new System.Windows.Forms.TextBox();
+            this.txtbxAddress = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblFirstName
@@ -109,11 +116,74 @@ namespace ProjectContactTracing
             this.lblAddress.TabIndex = 6;
             this.lblAddress.Text = "Address";
             // 
+            // txtbxFirstName
+            // 
+            this.txtbxFirstName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxFirstName.Location = new System.Drawing.Point(235, 30);
+            this.txtbxFirstName.Name = "txtbxFirstName";
+            this.txtbxFirstName.Size = new System.Drawing.Size(527, 32);
+            this.txtbxFirstName.TabIndex = 7;
+            // 
+            // txtbxMiddleInitial
+            // 
+            this.txtbxMiddleInitial.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxMiddleInitial.Location = new System.Drawing.Point(235, 80);
+            this.txtbxMiddleInitial.Name = "txtbxMiddleInitial";
+            this.txtbxMiddleInitial.Size = new System.Drawing.Size(527, 32);
+            this.txtbxMiddleInitial.TabIndex = 8;
+            // 
+            // txtbxLastName
+            // 
+            this.txtbxLastName.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxLastName.Location = new System.Drawing.Point(235, 130);
+            this.txtbxLastName.Name = "txtbxLastName";
+            this.txtbxLastName.Size = new System.Drawing.Size(527, 32);
+            this.txtbxLastName.TabIndex = 9;
+            // 
+            // txtbxAge
+            // 
+            this.txtbxAge.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxAge.Location = new System.Drawing.Point(235, 180);
+            this.txtbxAge.Name = "txtbxAge";
+            this.txtbxAge.Size = new System.Drawing.Size(527, 32);
+            this.txtbxAge.TabIndex = 10;
+            // 
+            // txtbxSex
+            // 
+            this.txtbxSex.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxSex.Location = new System.Drawing.Point(235, 230);
+            this.txtbxSex.Name = "txtbxSex";
+            this.txtbxSex.Size = new System.Drawing.Size(527, 32);
+            this.txtbxSex.TabIndex = 11;
+            // 
+            // txtbxContactNumber
+            // 
+            this.txtbxContactNumber.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxContactNumber.Location = new System.Drawing.Point(235, 280);
+            this.txtbxContactNumber.Name = "txtbxContactNumber";
+            this.txtbxContactNumber.Size = new System.Drawing.Size(527, 32);
+            this.txtbxContactNumber.TabIndex = 12;
+            // 
+            // txtbxAddress
+            // 
+            this.txtbxAddress.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbxAddress.Location = new System.Drawing.Point(235, 330);
+            this.txtbxAddress.Name = "txtbxAddress";
+            this.txtbxAddress.Size = new System.Drawing.Size(527, 32);
+            this.txtbxAddress.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtbxAddress);
+            this.Controls.Add(this.txtbxContactNumber);
+            this.Controls.Add(this.txtbxSex);
+            this.Controls.Add(this.txtbxAge);
+            this.Controls.Add(this.txtbxLastName);
+            this.Controls.Add(this.txtbxMiddleInitial);
+            this.Controls.Add(this.txtbxFirstName);
             this.Controls.Add(this.lblAddress);
             this.Controls.Add(this.lblContactNumber);
             this.Controls.Add(this.lblSex);
@@ -137,6 +207,13 @@ namespace ProjectContactTracing
         private System.Windows.Forms.Label lblSex;
         private System.Windows.Forms.Label lblContactNumber;
         private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtbxFirstName;
+        private System.Windows.Forms.TextBox txtbxMiddleInitial;
+        private System.Windows.Forms.TextBox txtbxLastName;
+        private System.Windows.Forms.TextBox txtbxAge;
+        private System.Windows.Forms.TextBox txtbxSex;
+        private System.Windows.Forms.TextBox txtbxContactNumber;
+        private System.Windows.Forms.TextBox txtbxAddress;
     }
 }
 
